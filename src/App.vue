fd0f822315978ce937567f860962f005475afb89
renatoalmeida49 <renatoalmeida49@gmail.com>
2021-05-10 17:26:21 -0300

    Reorganizacao de pastas

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script>
-import TheHeader from '@/components/TheHeader'
-import TheFooter from '@/components/TheFooter'
+import TheHeader from '@/components/Layout/TheHeader'
+import TheFooter from '@/components/Layout/TheFooter'
 
 export default {
   components: {
